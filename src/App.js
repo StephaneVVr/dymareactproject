20ef0868484f6f94ab948614baf66a30a7dd3b7e
Stéphane Vivier <stephane.vvr@gmail.com>
2019-07-25 11:36:20 +0200

    Add index.js in component folder for export components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import "./App.css";
+
+import { Header, MovieDetails, MovieList } from "./components";
 
 function App() {
-  return;
+  return (
+    <div className="App">
+      <Header />
+      <div className="d-flex flex-row">
+        <MovieList />
+        <MovieDetails />
+      </div>
+    </div>
+  );
 }
 
 export default App;
