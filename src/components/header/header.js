5d7678fcb3e31b68dc30f70b1dd7d13b95faa820
Stéphane Vivier <stephane.vvr@gmail.com>
2019-07-26 10:47:12 +0200

    Make header content component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <header className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          AlloMovie
+        </a>
+        <button className="navbar-toggler">
+          <span className="collapse navbar-collapse" />
+        </button>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Favoris
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
   }
 }
