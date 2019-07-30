7e4337a0bee00b29669c026725e7c7923b47b868
Stéphane Vivier <stephane.vvr@gmail.com>
2019-07-30 21:28:22 +0200

    FInish make basics coomponents

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class MovieDetails extends Component {
   render() {
-    return <div className="w-25">MovieDetails</div>;
+    return <div className="w-25 p-4">MovieDetails</div>;
   }
 }
--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import MovieElement from "./MovieElement";
 
 export default class MovieList extends Component {
   render() {
-    return <div className="w-75">MovieList</div>;
+    return (
+      <div className="w-75 d-flex flex-row flex-wrap align-content-start">
+        <MovieElement />
+        <MovieElement />
+        <MovieElement />
+        <MovieElement />
+        <MovieElement />
+        <MovieElement />
+      </div>
+    );
   }
 }
